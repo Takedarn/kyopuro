e1977ea409782c6b3283459a85faa9df25e5feef
Haruki Takeda <takedaharuki@jaist.ac.jp>
2025-01-14 16:47:20 +0900

    左と右の変数を用意するのがミソ

---
+++ chapter03/a11.py
@@ -0,0 +1,18 @@
+# input
+N, X = map(int, input().split())
+A = list(map(int, input().split()))
+
+
+# search
+l = 0
+r = N
+
+while True:
+    m = (l + r) // 2
+    if A[m] == X:
+        print(m + 1)
+        break
+    elif A[m] < X:
+        l = m
+    elif A[m] > X:
+        r = m
